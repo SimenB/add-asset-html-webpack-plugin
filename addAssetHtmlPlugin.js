cfbae5b25e0ecc5eeecf26961d34f5cf064809b2
Simen Bekkhus <sbekkhus91@gmail.com>
2016-02-27 17:40:12 +0100

    Use public path

--- addAssetHtmlPlugin.js
+++ addAssetHtmlPlugin.js
@@ -1,3 +1,17 @@
+import path from 'path'
+
+// Copied from html-webpack-plugin
+function resolvePublicPath (compilation, filename) {
+  let publicPath = typeof compilation.options.output.publicPath !== 'undefined'
+    ? compilation.options.output.publicPath
+    : path.relative(path.dirname(filename), '.')
+
+  if (publicPath.length && publicPath.substr(-1, 1) !== '/') {
+    publicPath += '/'
+  }
+  return publicPath
+}
+
 export default class AddAssetHtmlPlugin {
   constructor ({
     filename,
@@ -13,9 +27,11 @@ export default class AddAssetHtmlPlugin {
     compiler.plugin('compilation', (compilation) => {
       if (!this.filename) return compilation.errors.push(new Error('No filename defined'))
 
+      const publicPath = resolvePublicPath(compilation, this.filename)
+
       compilation.plugin('html-webpack-plugin-before-html-processing', (htmlPluginData, callback) => {
         htmlPluginData.plugin.addFileToAssets(this.filename, compilation)
-          .then((filename) => htmlPluginData.assets[this.typeOfAsset].unshift(`/${filename}`))
+          .then((filename) => htmlPluginData.assets[this.typeOfAsset].unshift(`${publicPath}${filename}`))
           .then(() => {
             if (this.includeSourcemap) {
               return htmlPluginData.plugin.addFileToAssets(`${this.filename}.map`, compilation)

1eed44c7b5c01abd4a70c3de6dc0ebc3d82df15f
Simen Bekkhus <sbekkhus91@gmail.com>
2016-07-05 22:46:05 +0200

    Use eslint-config-airbnb-base instead of standard

--- addAssetHtmlPlugin.js
+++ addAssetHtmlPlugin.js
@@ -1,42 +1,42 @@
-import path from 'path'
-import Promise from 'bluebird'
+import path from 'path';
+import Promise from 'bluebird';
 
 // Copied from html-webpack-plugin
-function resolvePublicPath (compilation, filename) {
+function resolvePublicPath(compilation, filename) {
   let publicPath = typeof compilation.options.output.publicPath !== 'undefined'
     ? compilation.options.output.publicPath
-    : path.relative(path.dirname(filename), '.')
+    : path.relative(path.dirname(filename), '.');
 
   if (publicPath.length && publicPath.substr(-1, 1) !== '/') {
-    publicPath += '/'
+    publicPath += '/';
   }
-  return publicPath
+  return publicPath;
 }
 
-function addFileToAssets (htmlPluginData, compilation, { filename, typeOfAsset = 'js', includeSourcemap = true } = {}) {
-  if (!filename) return compilation.errors.push(new Error('No filename defined'))
+function addFileToAssets(htmlPluginData, compilation, { filename, typeOfAsset = 'js', includeSourcemap = true } = {}) {
+  if (!filename) return compilation.errors.push(new Error('No filename defined'));
 
   return htmlPluginData.plugin.addFileToAssets(filename, compilation)
-    .then((filename) => htmlPluginData.assets[typeOfAsset].unshift(`${resolvePublicPath(compilation, filename)}${filename}`))
+    .then(addedFilename => htmlPluginData.assets[typeOfAsset].unshift(`${resolvePublicPath(compilation, addedFilename)}${addedFilename}`))
     .then(() => {
       if (includeSourcemap) {
-        return htmlPluginData.plugin.addFileToAssets(`${filename}.map`, compilation)
+        return htmlPluginData.plugin.addFileToAssets(`${filename}.map`, compilation);
       }
-      return null
-    })
+      return null;
+    });
 }
 
 export default class AddAssetHtmlPlugin {
-  constructor (assets = []) {
-    this.assets = Array.isArray(assets) ? assets.slice().reverse() : [assets]
+  constructor(assets = []) {
+    this.assets = Array.isArray(assets) ? assets.slice().reverse() : [assets];
   }
 
-  apply (compiler) {
-    compiler.plugin('compilation', (compilation) => {
+  apply(compiler) {
+    compiler.plugin('compilation', compilation => {
       compilation.plugin('html-webpack-plugin-before-html-generation', (htmlPluginData, callback) => {
         Promise.mapSeries(this.assets, asset => addFileToAssets(htmlPluginData, compilation, asset))
-          .then(() => callback(null, htmlPluginData))
-      })
-    })
+          .then(() => callback(null, htmlPluginData));
+      });
+    });
   }
 }

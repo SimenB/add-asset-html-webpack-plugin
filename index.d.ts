02ea0b44ae202f4ac000bcbb413fc2ccaebce499
Simen Bekkhus <sbekkhus91@gmail.com>
2016-07-27 12:08:33 +0200

    Add super basic typings file

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+// TODO: I don't know if it's possible to document default values...
+
+interface PluginOptions {
+  filename: string;
+  typeOfAsset?: string;
+  includeSourcemap?: boolean;
+  hash?: boolean;
+  publicPath?: string;
+}
+
+export class AddAssetHtmlPlugin {
+  constructor(options: PluginOptions);
+
+  apply(compiler): void;
+}

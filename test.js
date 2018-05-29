40b124490e105520933cbcfd73882331885b72e0
Simen Bekkhus <sbekkhus91@gmail.com>
2018-05-29 19:20:23 +0200

    chore: upgrade to jest 23 and babel 7

--- test.js
+++ test.js
@@ -75,7 +75,7 @@ test('should used passed in publicPath', async () => {
 });
 
 // TODO: No idea what this does, actually... Coverage currently hits it, but the logic is untested.
-test('should handle missing `publicPath`');
+test.skip('should handle missing `publicPath`', () => {});
 
 test('should add file missing "/" to public path', async () => {
   const compilation = { options: { output: { publicPath: 'vendor' } } };

8387886352d3c31bf1871b5f0b49f8da77e3fb62
Simen Bekkhus <sbekkhus91@gmail.com>
2022-03-08 08:53:55 +0100

    fix: remove the hash calculation if hash exists (#159)
    Co-authored-by: Ivan Vasilov <ivan@hfour.com>

--- src/index.js
+++ src/index.js
@@ -143,9 +143,14 @@ export default class AddAssetHtmlPlugin {
 
     let suffix = '';
     if (hash) {
-      const md5 = crypto.createHash('md5');
-      md5.update(compilation.assets[addedFilename].source());
-      suffix = `?${md5.digest('hex').substr(0, 20)}`;
+      // if the hash is set by html-webpack-plugin use that hash, else generate a new one
+      if (compilation.hash) {
+        suffix = `?${compilation.hash}`;
+      } else {
+        const md5 = crypto.createHash('md5');
+        md5.update(compilation.assets[addedFilename].source());
+        suffix = `?${md5.digest('hex').substr(0, 20)}`;
+      }
     }
 
     const resolvedPublicPath =
